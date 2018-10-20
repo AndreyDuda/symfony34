4e65049106c82cff6aa7506229fc21a31afdd7cf
andreew <andreyduda88@gmail.com>
2018-10-21 01:04:23 +0300

    add productController

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -8,8 +8,36 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
-class ProductController
+class ProductController extends Controller
 {
+    /**
+     * @Route("/products", name="product_list")
+     */
+    public function indexAction()
+    {
+        $products = [];
+
+        for ($i=0; $i<=10; $i++) {
+            $products[] = rand(1, 100);
+        }
+
+        $data = [
+            'products' => $products
+        ];
+
+        return $this->render('@App/product/index.html.twig', $data);
+    }
+
+    /**
+     * @Route("/product/{id}", name="product_item", requirements={"id": "[0-9]+"})
+     */
+    public function showAction(Request $request)
+    {
+        die($request->get('id'));
+    }
 
 }
